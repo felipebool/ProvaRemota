455f74969ad03ce8dd0c0267df2334e1aa7de6f1
felipe <bolzin@gmail.com>
2016-07-13 19:00:28 -0300

    almost there

--- UsefulTableTree.php
+++ UsefulTableTree.php
@@ -2,19 +2,15 @@
 class UsefulTableTree {
     public $header_fields;
     public $data;
+    public $debug;
     
-    // construtor da classe, recebe o array multidimencional
-    // e um array contendo os nomes das colunas da tabela
     function __construct($data, $header_fields) {
         $this->data = $data;
         $this->header_fields = $header_fields;
     }
 
-
-    // get_table simplesmente chama as outras funções que
-    // retornam as partes da tabela, junta em uma variável
-    // e retorna a tabela construída
     function get_table() {
+        $table = "";
         $table .= $this->get_table_header();
         $table .= $this->get_table_body();
         $table .= $this->get_table_footer();
@@ -22,9 +18,6 @@ class UsefulTableTree {
         return $table;
     }
 
-
-    // get_table_header constrói o cabeçalho da tabela de
-    // acordo com os nomes das colunas passados ao construtor
     private function get_table_header() {
         $table_header = "<table><thead><tr>";
 
@@ -36,38 +29,27 @@ class UsefulTableTree {
         return $table_header;    
     }
 
-    
-    // get_table_body é responsável por retornar o corpo da
-    // tabela, ele itera sobre o primeiro nível da estrutura
-    // de dados e passa cada uma das árvores para get_single_body.
-    // o valor retornado é concatenado e a função retorna o
-    // corpo da tabela
     private function get_table_body() {
         $table_body = "";
-        foreach ($this->data as $tree) {
-            $table_body .= $this->get_single_body($tree);
+        foreach ($this->data as $key => $value) {
+            $table_body .= $this->get_single_body(array($key => $value));
         }
 
         return $table_body;
     }
 
-
-    // get_single_body é a função auxiliar de get_table_body.
-    // ela constrói uma fila com a tabela (chamando queuefy_body)
-    // e itera sobre a fila retornada construindo o html de 
-    // um único body. eu explico melhor o pq do uso desta função
-    // na documentação do git
     private function get_single_body($data) {
-        $single_body .= "<tbody>";
-
-        $queued_body = $this->queuefy_body($data);
         $tr_element = "";
+        $single_body = "<tbody>";
+        $queued_body = array();
+        $queued_body2 = array();
+
+        $this->queuefy_body($data, $queued_body);
+        $max_level = $this->max_level($queued_body);
 
         foreach ($queued_body as $element) {
             foreach ($element as $key => $value) {
-                // para mais níveis, basta testar para o
-                // último nível, setado em queuefy_body
-                if ($key == 'lvl3') {
+                if ($key == $max_level) {
                     $tr_element  .= "<td>$value</td>";
                     $single_body .= "<tr>$tr_element</tr>";
                     $tr_element = "";
@@ -80,42 +62,54 @@ class UsefulTableTree {
 
         $single_body .= "</tbody>";
 
+        unset($queued_body);
         return $single_body;
     }
 
-
-    // get_table_footer foi criada basicamente pra suportar
-    // modificações futuras, nas quais seja necessário customizar
-    // o footer da tabela.
     private function get_table_footer() {
         return "</table>";
     }
 
-    // a tarefa da queuefy_body é simples, é uma busca em profundidade
-    // iterativa construindo uma fila com o valor de cada célula. na
-    // documentação eu explico melhor o pq destas escolhas
-    private function queuefy_body() {
-        $queued_body = array();
-        
-        foreach ($this->data as $lvl1 => $lvl1Value) {
-            array_push($queued_body, array('lvl1' => $lvl1));
-            foreach ($lvl1Value as $lvl2 => $lvl2Value) {
-                array_push($queued_body, array('lvl2' => $lvl2));
-                foreach ($lvl2Value as $lvl3) {
-                    array_push($queued_body, array('lvl3' => $lvl3));
+
+    private function queuefy_body($data, &$queue, $level=1) {
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                array_push($queue, array($level => $key));
+                $this->queuefy_body($value, $queue, $level + 1);
+            }
+            else {
+                array_push($queue, array($level => $value));
+            }
+        }
+    }
+
+    private function max_level($tree) {
+        $max_level = 0;
+
+        foreach ($tree as $item) {
+            foreach ($item as $key => $value) {
+                if ($key > $max_level) {
+                    $max_level = $key;
                 }
             }
         }
 
-        return $queued_body;
+        return $max_level;
     }
 }
 
+
 $data = array(
     'X' => array(
-        '1' => array('a', 'b', 'c')
+        '1' => array('a', 'b', 'c'),
+        '2' => array('d', 'e', 'f'),
+    ),
+    'Y' => array(
+        '3' => array('d', 'e', 'f'),
+        '4' => array('d', 'e', 'f')
     )
 );
 
 $pretty_table = new UsefulTableTree($data, array('cl1', 'cl2', 'cl3'));
 echo $pretty_table->get_table();
+
